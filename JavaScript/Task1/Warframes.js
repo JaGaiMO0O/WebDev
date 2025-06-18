7bc24f0104a46500a9031081381db74a2a25d79a
WebDev <mohammad-m-y@hotmail.com>
2025-06-18 19:39:16 +0300

    Warframe Dating

---
+++ JavaScript/Task1/Warframes.js
@@ -0,0 +1,38 @@
+export const frames = [
+    {
+        id: "1M",
+        image: "Ash.png",
+        name: "Ash",
+        gender: "Male"
+    },
+    {
+        id: "1F",
+        image: "Saryn.png",
+        name: "Saryn",
+        gender: "Female"
+    },
+    {
+        id: "2M",
+        image: "Frost.png",
+        name: "Frost",
+        gender: "Male"
+    },
+    {
+        id: "2F",
+        image: "Mag.png",
+        name: "Mag",
+        gender: "Female"
+    },
+    {
+        id: "3M",
+        image: "Volt.png",
+        name: "Volt",
+        gender: "Male"
+    },
+    {
+        id: "3F",
+        image: "Nova.png",
+        name: "Nova",
+        gender: "Female"
+    }
+]
