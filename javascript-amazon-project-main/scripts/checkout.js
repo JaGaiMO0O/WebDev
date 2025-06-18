2804f30d3249c159b63ffff04f2ccf91248d3012
WebDev <mohammad-m-y@hotmail.com>
2025-06-18 13:53:16 +0300

    LoadCart from Backend + Promises

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -2,9 +2,46 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import '../data/cart-class.js';
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve();
+        });
+    }),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    })
 
+]).then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+
+/*
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve('value1');
+    });
+}).then((value) => {
+    return new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        })
+    })
+}).then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+
+*/
+
+/*
 loadProducts(() => {
     renderOrderSummary();
     renderPaymentSummary();
 });
+*/
